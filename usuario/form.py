08744808d0f1a4730ea7dede30f7863d18c32539
gerson <jrf_95@hotmail.com>
2017-07-21 22:44:42 -0500

    validando formulario-part1

--- usuario/form.py
+++ usuario/form.py
@@ -3,12 +3,14 @@ from django import forms
 from .models import User , UserProfile , ProfileImage , Category
 
 class FormularioUsuario(forms.ModelForm):
+	password = forms.CharField(max_length=30,min_length=7,widget=forms.PasswordInput(),error_messages={'required':'Ingresa correctamente el password'})
 	class Meta:
 		model = User
 		fields = ["username","email","password"]
-		widgets = {
-			"password":forms.TextInput(attrs={"type":"password"})
-		}
+#otra forma de hacer los widgets			
+#		widgets = {
+#			"password":forms.TextInput(attrs={"type":"password"})
+#		}
 		def clean_email(self):
 			email = self.cleaned_data.get("email")
 			nombre , proveedor = email.split("@")
@@ -16,12 +18,14 @@ class FormularioUsuario(forms.ModelForm):
 			if extencion == "com":
 				raise forms.ValidationError("Escribe correctamente tu correo")
 			return email	
-
-
+		
 
 
 class FormularioPerfil(forms.ModelForm):
+	companyname = forms.CharField(max_length=30)
 	categoria = forms.ModelChoiceField(queryset=Category.objects.all(),initial=0)
+	companyconcept = forms.CharField(min_length=200,widget=forms.Textarea() )
+	contact = forms.CharField(min_length=9, max_length=9 )
 	class Meta:
 		model = UserProfile
 		fields = ["companyname","companyconcept","contact","companyservice"]
@@ -32,8 +36,8 @@ class FormularioImagen(forms.ModelForm):
 		model = ProfileImage
 		fields = ["companyimage","biography"]
 		widgets = {
-				'companyimage' : forms.FileInput(attrs = {'accept': 'image/gif, image/jpeg, image/png'}),
-				'biography' : forms.FileInput(attrs = {'accept': 'image/gif, image/jpeg, image/png'}),
+				'companyimage' : forms.FileInput(attrs = {'accept': 'image/gif, image/jpeg, image/png','required':'Necesitas una imagen para tu perfil'}),
+				'biography' : forms.FileInput(attrs = {'accept': 'image/gif, image/jpeg, image/png','required':'Necesitas una imagen de biografia'}),
 		}
 
 		
